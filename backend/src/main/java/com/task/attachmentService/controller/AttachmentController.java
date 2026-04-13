93be3539cd80c0a23c244a5dc1124fb172994142
Maheshwarinarnaware23 <maheshwarinarnaware@gmail.com>
2026-04-13 21:27:36 +0530

    Add CRUD operations to AttachmentController

--- backend/src/main/java/com/task/attachmentService/controller/AttachmentController.java
+++ backend/src/main/java/com/task/attachmentService/controller/AttachmentController.java
@@ -1,23 +1,48 @@
 package com.task.attachmentService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.task.attachmentService.entitiy.Attachment;
 import com.task.attachmentService.service.AttachmentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/attachments")
 public class AttachmentController {
 
     @Autowired
-    private AttachmentService  service;
+    private AttachmentService service;
 
+    // CREATE
     @PostMapping
     public Attachment add(@RequestBody Attachment a) {
         return service.save(a);
+    }
+
+    // GET ALL
+    @GetMapping
+    public List<Attachment> getAll() {
+        return service.getAll();
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public Attachment getById(@PathVariable Integer id) {
+        return service.getById(id);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Integer id) {
+        service.delete(id);
+        return "Attachment deleted successfully";
+    }
+
+    // GET BY TASK
+    @GetMapping("/task/{taskId}")
+    public List<Attachment> getByTask(@PathVariable Integer taskId) {
+        return service.getByTaskId(taskId);
     }
 }
