d71855d6c95696f8de8e854db0966c03029fc346
Maheshwarinarnaware23 <maheshwarinarnaware@gmail.com>
2026-04-13 21:26:11 +0530

    Implement CRUD operations in AttachmentService

--- backend/src/main/java/com/task/attachmentService/service/AttachmentService.java
+++ backend/src/main/java/com/task/attachmentService/service/AttachmentService.java
@@ -6,13 +6,36 @@ import org.springframework.stereotype.Service;
 import com.task.attachmentService.entitiy.Attachment;
 import com.task.attachmentService.repository.AttachmentRepository;
 
+import java.util.List;
+
 @Service
 public class AttachmentService {
 
     @Autowired
     private AttachmentRepository repo;
 
+    // CREATE
     public Attachment save(Attachment a) {
         return repo.save(a);
+    }
+
+    // GET ALL
+    public List<Attachment> getAll() {
+        return repo.findAll();
+    }
+
+    // GET BY ID
+    public Attachment getById(Integer id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    // DELETE
+    public void delete(Integer id) {
+        repo.deleteById(id);
+    }
+
+    // GET BY TASK ID
+    public List<Attachment> getByTaskId(Integer taskId) {
+        return repo.findByTask_TaskID(taskId);
     }
 }
