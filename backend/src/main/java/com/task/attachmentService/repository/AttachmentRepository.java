65b66417e8998a2c57f94d4acc5c3b4b9485b58f
Maheshwari Narnaware <maheshwarinarnaware@gmail.com>
2026-04-16 12:57:40 +0530

    comment attachment test update

--- backend/src/main/java/com/task/attachmentService/repository/AttachmentRepository.java
+++ backend/src/main/java/com/task/attachmentService/repository/AttachmentRepository.java
@@ -3,11 +3,15 @@ package com.task.attachmentService.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 import com.task.attachmentService.entitiy.Attachment;
-import java.util.List;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
-   List<Attachment> findByTask_TaskID(Integer taskID);
+
+    // CORRECT (based on your Task entity)
+    List<Attachment> findByTask_TaskID(Integer taskID);
+
+    int countByTask_TaskID(Integer taskID);
+
+    void deleteByTask_TaskID(Integer taskID);
 }
